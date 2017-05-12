a823328ba2a8165abbb14a56d9cde6e925fe9fd3
MoLice <sf.molice@gmail.com>
2017-05-12 15:06:04 +0800

    First Commit

---
+++ TestSpec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSpec
+//
+//  Created by MoLice on 2017/5/12.
+//  Copyright © 2017年 MoLice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestSpec/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestSpec
+//
+//  Created by MoLice on 2017/5/12.
+//  Copyright © 2017年 MoLice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
